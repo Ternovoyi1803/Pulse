81b29afe635482d38942d4c0e41458e4a42f75f7
Ivan <bosmam333@gmail.com>
2020-07-30 22:47:02 +0300

    add php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,20 @@ $(document).ready(function(){
     validate('.consultation form');
 
     $('[name="phone"]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#order, #consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn();
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
   });
